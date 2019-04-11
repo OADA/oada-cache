a5ce1f35a1641400eea320b0643027195bdf5cad
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-04-10 21:03:31 -0400

    add in-memory cache PUT unit test

--- test/memorycache.test.js
+++ test/memorycache.test.js
@@ -10,19 +10,21 @@ const { token, domain } = require("./config");
 const { tree, putResource, getConnections } = require("./utils.js");
 const { performance } = require("perf_hooks");
 
-var connection;
-var resources = [];
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+const cleanMemoryTimer = 11000;
+const dbPutDelay = 6000;
 
 describe(`In-memory Cache`, async function() {
+  this.timeout(30000);
   describe(`GET`, async function() {
+    var connection;
     before(`Create connection`, async function() {
       connection = await oada.connect({
         domain,
         token: "def",
       });
     });
-
-    const timer = ms => new Promise(res => setTimeout(res, ms));
 
     it(`Should get a resource from server`, async function() {
       var response = await connection.get({
@@ -37,9 +39,8 @@ describe(`In-memory Cache`, async function() {
       );
     });
 
-    it(`Should wait 20000 ms`, async function() {
-      this.timeout(30000);
-      await timer(20000);
+    it(`Should wait ${cleanMemoryTimer} ms`, async function() {
+      await timer(cleanMemoryTimer);
     });
 
     it(`In-memory cache should be empty`, async function() {
@@ -66,7 +67,54 @@ describe(`In-memory Cache`, async function() {
       expect(response.data).to.include.keys(["_id", "_rev", "_type", "_meta"]);
     });
 
-    it(`Should close the connection gracefully`, async function() {
+    after(`Close connection`, async function() {
+      await timer(cleanMemoryTimer);
+      connection.disconnect();
+    });
+  });
+
+  describe(`PUT`, async function() {
+    var connection;
+    before(`Create connection`, async function() {
+      connection = await oada.connect({
+        domain,
+        token: "def",
+      });
+    });
+
+    const timer = ms => new Promise(res => setTimeout(res, ms));
+
+    it(`Should create a resource`, async function() {
+      var response = await connection.put({
+        path: "/bookmarks/test",
+        data: { sometest: 123 },
+        tree,
+      });
+      expect(response.status).to.equal(204);
+      expect(response.headers).to.include.keys([
+        "content-location",
+        "x-oada-rev",
+        "location",
+      ]);
+    });
+
+    it(`The resource should be waiting for PUT`, async function() {
+      var data = connection._getMemoryCache();
+      expect(data[Object.keys(data)[0]]).to.include.keys(["promise"]);
+    });
+
+    it(`The resource should NOT be waiting for PUT`, async function() {
+      await timer(dbPutDelay);
+      var data = connection._getMemoryCache();
+      expect(data[Object.keys(data)[0]]).to.not.include.keys(["promise"]);
+    });
+
+    it(`In-memory cache should be empty`, async function() {
+      await timer(cleanMemoryTimer);
+      expect(connection._getMemoryCache()).to.be.empty;
+    });
+
+    after(`Close connection`, async function() {
       connection.disconnect();
     });
   });
