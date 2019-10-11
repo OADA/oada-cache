b54475cb5f43181bd45d00aef0b392dfb1d9d98c
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-10-11 12:29:23 -0400

    fixed 2 failing test cases in startup.test.js

--- test/startup.test.js
+++ test/startup.test.js
@@ -4,9 +4,7 @@ import chai from "chai";
 var expect = chai.expect;
 const status = require("http-status");
 
-let token = "def";
-//let domain = "https://vip3.ecn.purdue.edu";
-let domain = "https://localhost";
+const { token, domain } = require("./config.js");
 let connections = new Array(4);
 let contentType = "application/vnd.oada.yield.1+json";
 let connectTime = 30 * 1000; // seconds to click through oauth
@@ -24,8 +22,8 @@ let connectionParameters = {
     redirect: "http://localhost:8000/oauth2/redirect.html",
     metadata:
       "eyJqa3UiOiJodHRwczovL2lkZW50aXR5Lm9hZGEtZGV2LmNvbS9jZXJ0cyIsImtpZCI6ImtqY1NjamMzMmR3SlhYTEpEczNyMTI0c2ExIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJyZWRpcmVjdF91cmlzIjpbImh0dHA6Ly92aXAzLmVjbi5wdXJkdWUuZWR1OjgwMDAvb2F1dGgyL3JlZGlyZWN0Lmh0bWwiLCJodHRwOi8vbG9jYWxob3N0OjgwMDAvb2F1dGgyL3JlZGlyZWN0Lmh0bWwiXSwidG9rZW5fZW5kcG9pbnRfYXV0aF9tZXRob2QiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6Y2xpZW50LWFzc2VydGlvbi10eXBlOmp3dC1iZWFyZXIiLCJncmFudF90eXBlcyI6WyJpbXBsaWNpdCJdLCJyZXNwb25zZV90eXBlcyI6WyJ0b2tlbiIsImlkX3Rva2VuIiwiaWRfdG9rZW4gdG9rZW4iXSwiY2xpZW50X25hbWUiOiJPcGVuQVRLIiwiY2xpZW50X3VyaSI6Imh0dHBzOi8vdmlwMy5lY24ucHVyZHVlLmVkdSIsImNvbnRhY3RzIjpbIlNhbSBOb2VsIDxzYW5vZWxAcHVyZHVlLmVkdT4iXSwic29mdHdhcmVfaWQiOiIxZjc4NDc3Zi0zNTQxLTQxM2ItOTdiNi04NjQ0YjRhZjViYjgiLCJyZWdpc3RyYXRpb25fcHJvdmlkZXIiOiJodHRwczovL2lkZW50aXR5Lm9hZGEtZGV2LmNvbSIsImlhdCI6MTUxMjAwNjc2MX0.AJSjNlWX8UKfVh-h1ebCe0MEGqKzArNJ6x0nmta0oFMcWMyR6Cn2saR-oHvU8WrtUMEr-w020mAjvhfYav4EdT3GOGtaFgnbVkIs73iIMtr8Z-Y6mDEzqRzNzVRMLghj7CyWRCNJEk0jwWjOuC8FH4UsfHmtw3ouMFomjwsNLY0",
-    scope: "oada.yield:all"
-  }
+    scope: "oada.yield:all",
+  },
 };
 
 describe("~~~~~~ Testing Connect() -> Disconnect() -> Connect() ~~~~~~~", function() {
@@ -34,9 +32,9 @@ describe("~~~~~~ Testing Connect() -> Disconnect() -> Connect() ~~~~~~~", functi
     connections[0] = await oada.connect({
       domain,
       token: "def",
-      options: connectionParameters.options
+      options: connectionParameters.options,
     });
-    expect(connections[0]).to.have.keys([
+    expect(connections[0]).to.contain.all.keys([
       "token",
       "disconnect",
       "reconnect",
@@ -46,10 +44,10 @@ describe("~~~~~~ Testing Connect() -> Disconnect() -> Connect() ~~~~~~~", functi
       "delete",
       "resetCache",
       "cache",
-      "websocket"
+      "websocket",
     ]);
-    expect(connections[0].cache).to.equal(true);
-    expect(connections[0].websocket).to.equal(true);
+    expect(connections[0].cache).to.be.an("object");
+    expect(connections[0].websocket).to.be.an("object");
     expect(connections[0].get).to.satisfy(member => {
       return typeof member === "function";
     });
@@ -81,9 +79,9 @@ describe("~~~~~~ Testing Connect() -> Disconnect() -> Connect() ~~~~~~~", functi
     connections[0] = await oada.connect({
       domain,
       token: "def",
-      options: connectionParameters.options
+      options: connectionParameters.options,
     });
-    expect(connections[0]).to.have.keys([
+    expect(connections[0]).to.contain.all.keys([
       "token",
       "disconnect",
       "reconnect",
@@ -93,10 +91,10 @@ describe("~~~~~~ Testing Connect() -> Disconnect() -> Connect() ~~~~~~~", functi
       "delete",
       "resetCache",
       "cache",
-      "websocket"
+      "websocket",
     ]);
-    expect(connections[0].cache).to.equal(true);
-    expect(connections[0].websocket).to.equal(true);
+    expect(connections[0].cache).to.be.an("object");
+    expect(connections[0].websocket).to.be.an("object");
     expect(connections[0].get).to.satisfy(member => {
       return typeof member === "function";
     });
@@ -126,10 +124,10 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
     async function() {
       connection = await oada.connect({
         domain,
-        token: "def"
+        token: "def",
       });
       conn = connection;
-    }
+    },
   );
 
   before("Disconnecting - First time", function() {
@@ -141,10 +139,10 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
     async function() {
       connection = await oada.connect({
         domain,
-        token: "def"
+        token: "def",
       });
       conn = connection;
-    }
+    },
   );
 
   it(`Should error when neither 'url' nor 'path' are supplied`, async function() {
@@ -152,7 +150,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
       var response = await conn.put({
         data: `"123"`,
         tree,
-        type: "application/json"
+        type: "application/json",
       });
     } catch (error) {
       expect(error.message).to.equal("Either path or url must be specified.");
@@ -163,7 +161,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
     try {
       var response = await conn.put({
         path: "/bookmarks/testA/sometest",
-        data: { _type: "application/json" }
+        data: { _type: "application/json" },
       });
       expect(response.status).to.equal(status.NO_CONTENT);
     } catch (error) {
@@ -175,7 +173,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
     var response = await conn.put({
       path: "/bookmarks/testA/somethingnew",
       data: `"abc123"`,
-      type: "application/json"
+      type: "application/json",
     });
     expect(response.status).to.equal(status.NO_CONTENT);
   });
@@ -184,7 +182,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
     var response = await conn.put({
       path: "/bookmarks/testA/somethingnew",
       data: `"abc123"`,
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
     });
     expect(response.status).to.equal(status.NO_CONTENT);
   });
@@ -194,145 +192,145 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
       path:
         "/bookmarks/test/aaa/bbb/index-one/ccc/index-two/ddd/index-three/eee/test/123",
       type: "application/vnd.oada.as-harvested.yield-moisture.dataset.1+json",
-      data: `"some test"`
+      data: `"some test"`,
     });
     expect(response.status).to.equal(204);
     expect(response.headers).to.include.keys([
       "content-location",
       "x-oada-rev",
-      "location"
+      "location",
     ]);
   });
 
   it(`Should create the data PUT in the previous test.`, async function() {
     var response = await conn.get({
-      path: "/bookmarks/test"
+      path: "/bookmarks/test",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: aaa", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa"
+      path: "/bookmarks/test/aaa",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: bbb", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb"
+      path: "/bookmarks/test/aaa/bbb",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: index-one", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one"
+      path: "/bookmarks/test/aaa/bbb/index-one",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: ccc", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one/ccc"
+      path: "/bookmarks/test/aaa/bbb/index-one/ccc",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: index-two", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one/ccc/index-two"
+      path: "/bookmarks/test/aaa/bbb/index-one/ccc/index-two",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: ddd", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one/ccc/index-two/ddd"
+      path: "/bookmarks/test/aaa/bbb/index-one/ccc/index-two/ddd",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: test", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test"
+      path: "/bookmarks/test",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: aaa", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa"
+      path: "/bookmarks/test/aaa",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: bbb", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb"
+      path: "/bookmarks/test/aaa/bbb",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: index-one", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one"
+      path: "/bookmarks/test/aaa/bbb/index-one",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
   it("retrieving previous data: ccc", async function() {
     var response = await conn.get({
-      path: "/bookmarks/test/aaa/bbb/index-one/ccc"
+      path: "/bookmarks/test/aaa/bbb/index-one/ccc",
     });
     expect(response.status).to.equal(200);
     expect(response.headers).to.include.keys([
       "content-location",
-      "x-oada-rev"
+      "x-oada-rev",
     ]);
   });
 
@@ -343,12 +341,12 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
         path: "/bookmarks/test",
         watch: payload => {
           console.log("handlinh watch valid", payload);
-        } // handleWatch("exists")
+        }, // handleWatch("exists")
       });
       expect(response.status).to.equal(status.OK);
       expect(response.headers).to.include.keys([
         "content-location",
-        "x-oada-rev"
+        "x-oada-rev",
       ]);
     } catch (error) {
       console.log("Catch error", error);
@@ -362,7 +360,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> puts ~~~~~~~", () => {
         path: "/bookmarks/test/aaa/bbb/index-one/yyy",
         watch: payload => {
           console.log("handlinng watch invalid", payload);
-        } // handleWatch("exists")//handleWatch("does not exist")
+        }, // handleWatch("exists")//handleWatch("does not exist")
         //Possible EventEmitter memory leak detected. 11 destroyed listeners added.
       });
     } catch (error) {
@@ -385,10 +383,10 @@ describe("~~~~connect() -> disconnect() -> connect() -> disconnect() -> puts ~~~
     async function() {
       connection = await oada.connect({
         domain,
-        token: "def"
+        token: "def",
       });
       conn = connection;
-    }
+    },
   );
 
   before("Disconnecting - First time", function() {
@@ -400,10 +398,10 @@ describe("~~~~connect() -> disconnect() -> connect() -> disconnect() -> puts ~~~
     async function() {
       connection = await oada.connect({
         domain,
-        token: "def"
+        token: "def",
       });
       conn = connection;
-    }
+    },
   );
 
   before("Disconnecting - Second time", function() {
@@ -415,7 +413,7 @@ describe("~~~~connect() -> disconnect() -> connect() -> disconnect() -> puts ~~~
       var response = await conn.put({
         data: `"123"`,
         tree,
-        type: "application/json"
+        type: "application/json",
       });
     } catch (error) {
       expect(error.message).to.equal("Either path or url must be specified.");
